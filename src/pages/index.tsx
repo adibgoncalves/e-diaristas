0474076f7fff547312dfafd1605af3ed118530ef
Adib Gonçalves <adib.fernandes@gmail.com>
2021-06-18 22:10:54 -0300

    Metade da aula 02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/Inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -12,12 +19,65 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <UserInformation
-        name={"Adib Gonçalves"}
-        picture={"https://github.com/adibgoncalves.png"}
-        rating={3}
-        description={"Natal"}
-      />
+
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}>CEP inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Adib Gonçalves"}
+              picture={"https://github.com/adibgoncalves.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Adib Gonçalves"}
+              picture={"https://github.com/adibgoncalves.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Adib Gonçalves"}
+              picture={"https://github.com/adibgoncalves.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Adib Gonçalves"}
+              picture={"https://github.com/adibgoncalves.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Adib Gonçalves"}
+              picture={"https://github.com/adibgoncalves.png"}
+              rating={3}
+              description={"Natal"}
+            />
+            <UserInformation
+              name={"Adib Gonçalves"}
+              picture={"https://github.com/adibgoncalves.png"}
+              rating={3}
+              description={"Natal"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
